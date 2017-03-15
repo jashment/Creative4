62cc9cc3016011be16e10bc79ff1adb5abc214a9
Jashment22 <jashment@studentbody.byu.edu>
2017-03-15 19:43:01 +0000

    Working drawing app

--- app.js
+++ app.js
@@ -33,7 +33,7 @@ app.configure('production', function() {
 
 app.get('/', routes.index);
 
-app.listen(3000);
+app.listen(3010);
 console.log('Listening %d in %s mode', app.address().port, app.settings.env);
 
 io.sockets.on('connection', function(socket) {
--- routes/index.js
+++ routes/index.js
@@ -4,5 +4,5 @@
  */
 
 exports.index = function(req, res){
-  res.render('index', { title: 'Canvas' })
+  res.render('index', { title: 'Creative4' })
 };
